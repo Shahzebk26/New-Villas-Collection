306a01c2c8e1ea10fd466bcf8e22abbe82a5ee6b
asfandtaimoor <asfandtaimoor786@gmail.com>
2025-03-11 09:39:31 +0500

    DONE : Sidebar udpated

--- dumysidebarscript.js
+++
@@ -1,9 +0,0 @@
-// Open Sidebar
-function openSidebar() {
-    document.getElementById("sidebar").style.width = "250px";
-}
-
-// Close Sidebar
-function closeSidebar() {
-    document.getElementById("sidebar").style.width = "0";
-}
---
+++ script/app.js
@@ -0,0 +1,9 @@
+// Open Sidebar
+function openSidebar() {
+  document.getElementById('sidebar').classList.add('active');
+}
+
+// Close Sidebar
+function closeSidebar() {
+  document.getElementById('sidebar').classList.remove('active');
+}
